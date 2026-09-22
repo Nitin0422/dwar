7bf5b836f0b7bdaee06c7812b5cd52511ff8bd30
Nitin0422 <nitin.tandukar0422@gmail.com>
2026-09-22 07:58:53 +0545

    feat: add flags admin screens
    Refs T09

--- app/controllers/dwar/admin/flags_controller.rb
+++ app/controllers/dwar/admin/flags_controller.rb
@@ -3,7 +3,55 @@
 module Dwar
   module Admin
     class FlagsController < BaseController
+      before_action :set_flag, only: %i[edit update destroy]
+
       def index
+        @flags = Dwar::Flag.includes(:groups).order(:key)
+        q = params[:q]
+        if q.present?
+          pattern = "%#{ActiveRecord::Base.sanitize_sql_like(q)}%"
+          @flags = @flags.where("dwar_flags.key LIKE :q OR dwar_flags.description LIKE :q", q: pattern)
+        end
+      end
+
+      def new
+        @flag = Dwar::Flag.new
+      end
+
+      def create
+        @flag = Dwar::Flag.new(flag_params)
+
+        if @flag.save
+          redirect_to admin_flags_path, notice: "Flag was successfully created."
+        else
+          render :new, status: :unprocessable_entity
+        end
+      end
+
+      def edit
+      end
+
+      def update
+        if @flag.update(flag_params)
+          redirect_to admin_flags_path, notice: "Flag was successfully updated."
+        else
+          render :edit, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @flag.destroy
+        redirect_to admin_flags_path, notice: "Flag was successfully destroyed."
+      end
+
+      private
+
+      def set_flag
+        @flag = Dwar::Flag.find(params[:id])
+      end
+
+      def flag_params
+        params.require(:flag).permit(:key, :description, :state, :percentage, group_ids: [])
       end
     end
   end
